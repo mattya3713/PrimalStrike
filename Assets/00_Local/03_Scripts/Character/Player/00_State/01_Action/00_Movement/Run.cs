17ca59066193acde9d90c8eb2d81242bef5d2089
faeabhgafaafae <mattya3713gpsibou@gmail.com>
2025-10-07 16:31:00 +0900

    PlayerState Add
    プレイヤーステート追加中

---
+++ Assets/00_Local/03_Scripts/Character/Player/00_State/01_Action/00_Movement/Run.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerState
+{
+public class Run : MonoBehaviour
+{
+    void Start()
+    {   
+    }
+
+    void Update()
+    {
+        
+    }
+}
+}Å@// PlayerState.
